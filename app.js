17b30e76748eadb91713bf9bafd9d19e90eceedc
deviant <mb.deviant@gmail.com>
2024-04-26 22:25:46 +0300

    connect to database via mongoose

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 require("dotenv").config();
 
 var app = express();
+
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("connected to database");
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
