eae84ce5092e8562c5b11a0fb15e3690414642cc
deviant <mb.deviant@gmail.com>
2024-04-28 04:04:08 +0300

    create brand data model

---
+++ models/brand.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+  name: { type: string, required: true, minLength: 3, maxLenght: 40 },
+});
+
+BrandSchema.virtual("url").get(function () {
+  return `brand/${this.id}`;
+});
+
+module.exports = mongoose.model("Brand", BrandSchema);
+0;
