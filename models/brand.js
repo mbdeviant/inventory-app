e1715e4dce5a6bcc07a52c75f79469a51e68f50f
deviant <mb.deviant@gmail.com>
2024-04-28 20:21:47 +0300

    fix typo in brand name type

--- models/brand.js
+++ models/brand.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BrandSchema = new Schema({
-  name: { type: string, required: true, minLength: 3, maxLenght: 40 },
+  name: { type: String, required: true, minLength: 3, maxLenght: 40 },
 });
 
 BrandSchema.virtual("url").get(function () {
