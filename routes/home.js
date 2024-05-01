e319418d87c8cfdfaa80dbea2c6a440e90746800
deviant <mb.deviant@gmail.com>
2024-05-01 21:02:02 +0300

    create initial home route

---
+++ routes/home.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const brand_controller = require("../controllers/brandController");
+const category_controller = require("../controllers/categoryController");
+const product_controller = require("../controllers/categoryController");
