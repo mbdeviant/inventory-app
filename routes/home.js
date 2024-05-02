5c7905218e11703f380b501f0b2c2613af0a09c4
deviant <mb.deviant@gmail.com>
2024-05-02 03:20:27 +0300

    get product controller index on / route

--- routes/home.js
+++ routes/home.js
@@ -3,4 +3,8 @@ const router = express.Router();
 
 const brand_controller = require("../controllers/brandController");
 const category_controller = require("../controllers/categoryController");
-const product_controller = require("../controllers/categoryController");
+const product_controller = require("../controllers/productController");
+
+router.get("/", product_controller.index);
+
+module.exports = router;
