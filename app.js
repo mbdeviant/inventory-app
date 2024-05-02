6ecea1d343d707f6bce645d3e3b858d0b8b05863
deviant <mb.deviant@gmail.com>
2024-05-02 03:18:20 +0300

    add home route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const homeRouter = require("./routes/home");
 require("dotenv").config();
 
 var app = express();
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/home", homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
