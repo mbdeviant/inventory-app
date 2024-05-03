777ee5c07b731fcb0350727cca84563713e8b82f
deviant <mb.deviant@gmail.com>
2024-05-03 05:20:41 +0300

    create home page controller function

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,5 +6,16 @@ const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.render("index", { title: "hellomoafakis" });
+  const [numProducts, numBrands, numCategories] = await Promise.all([
+    Product.countDocuments({}).exec(),
+    Brand.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Inventory Home",
+    product_count: numProducts,
+    brand_count: numBrands,
+    category_count: numCategories,
+  });
 });
