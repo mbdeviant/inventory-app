b5e01826e8afd47983e7cd8d3186d4717e29c4b6
deviant <mb.deviant@gmail.com>
2024-05-08 23:15:07 +0300

    add routes for get and post methods of new brand form

--- routes/home.js
+++ routes/home.js
@@ -12,9 +12,10 @@ router.get("/products", product_controller.product_list);
 router.get("/product/:id", product_controller.product_detail);
 
 // Brand controllers
-router.get("/brands", brand_controller.brand_list);
 router.get("/brand/create", brand_controller.brand_create_get);
+router.post("/brand/create", brand_controller.brand_create_post);
 router.get("/brand/:id", brand_controller.brand_detail);
+router.get("/brands", brand_controller.brand_list);
 
 router.get("/categories", category_controller.category_list);
 router.get("/category/:id", category_controller.category_detail);
