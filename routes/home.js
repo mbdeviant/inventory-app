1a747b6c180a99b88f89f5cd9f666afc7058b4e7
deviant <mb.deviant@gmail.com>
2024-05-08 21:36:49 +0300

    add get method for brand form

--- routes/home.js
+++ routes/home.js
@@ -13,6 +13,7 @@ router.get("/product/:id", product_controller.product_detail);
 
 // Brand controllers
 router.get("/brands", brand_controller.brand_list);
+router.get("/brand/create", brand_controller.brand_create_get);
 router.get("/brand/:id", brand_controller.brand_detail);
 
 router.get("/categories", category_controller.category_list);
