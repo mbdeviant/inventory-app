dc9d14ad416c1a4e9ddd9e953cb6521ee6950e99
deviant <mb.deviant@gmail.com>
2024-05-14 08:11:18 +0300

    create product form post method

--- controllers/productController.js
+++ controllers/productController.js
@@ -62,12 +62,76 @@ exports.product_create_get = asyncHandler(async (req, res, next) => {
     Category.find().sort({ name: 1 }).exec(),
   ]);
 
-  console.log(allbrands);
-  console.log(allCategories);
-
   res.render("product_form", {
     title: "Add new product",
     brands: allbrands,
     categories: allCategories,
   });
 });
+
+exports.product_create_post = [
+  body("name", "Name cannot be empty.").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description cannot be empty.")
+    .trim()
+    .isLength({ max: 250 })
+    .escape(),
+  body("category", "Category must be selected")
+    .not()
+    .isEmpty()
+    .withMessage("Please select a category"),
+  body("price", "Price cannot be empty")
+    .trim()
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a valid number"),
+  body("quantity", "Quantity cannot be empty")
+    .trim()
+    .isInt({ min: 0 })
+    .withMessage("Quantity must be a valid number"),
+  body("brand", "Brand must be selected")
+    .not()
+    .isEmpty()
+    .withMessage("Please select a brand"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      brand: req.body.brand,
+    });
+
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      const [allBrands, allCategories] = await Promise.all([
+        Brand.find().sort({ name: 1 }).exec(),
+        Category.find().sort({ name: 1 }).exec(),
+      ]);
+
+      res.render("product_form", {
+        title: "Add new product",
+        brands: allBrands,
+        categories: allCategories,
+        product: product,
+        errors: errors.array(),
+      });
+
+      return;
+    } else {
+      await product.save();
+      res.redirect(product.url);
+    }
+  }),
+];
+
+// const ProductSchema = new Schema({
+//   name: { type: String, required: true, maxLenght: 100 },
+//   description: { type: String, maxLenght: 250 },
+//   category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+//   price: Number,
+//   quantity: Number,
+//   brand: { type: Schema.Types.ObjectId, ref: "Brand" },
+// });
