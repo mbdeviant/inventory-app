345f9f84832e62d3854e489d2c66f8cd71e1f25a
deviant <mb.deviant@gmail.com>
2024-05-29 21:57:30 +0300

    remove comment

--- controllers/productController.js
+++ controllers/productController.js
@@ -125,12 +125,3 @@ exports.product_create_post = [
     }
   }),
 ];
-
-// const ProductSchema = new Schema({
-//   name: { type: String, required: true, maxLenght: 100 },
-//   description: { type: String, maxLenght: 250 },
-//   category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
-//   price: Number,
-//   quantity: Number,
-//   brand: { type: Schema.Types.ObjectId, ref: "Brand" },
-// });
