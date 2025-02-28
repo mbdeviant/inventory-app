fdcdd7663d0f2a3905b7a7b9d147a44af02e5e3f
deviant <mb.deviant@gmail.com>
2025-02-28 04:20:39 +0300

    add middleware to delete methods

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/auth");
 
 const brand_controller = require("../controllers/brandController");
 const category_controller = require("../controllers/categoryController");
@@ -11,7 +12,11 @@ router.get("/", product_controller.index);
 router.get("/product/create", product_controller.product_create_get);
 router.post("/product/create", product_controller.product_create_post);
 router.get("/product/:id/delete", product_controller.product_delete_get);
-router.post("/product/:id/delete", product_controller.product_delete_post);
+router.post(
+  "/product/:id/delete",
+  authMiddleware,
+  product_controller.product_delete_post
+);
 router.get("/product/:id/update", product_controller.product_update_get);
 router.post("/product/:id/update", product_controller.product_update_post);
 router.get("/product/:id", product_controller.product_detail);
@@ -19,7 +24,11 @@ router.get("/products", product_controller.product_list);
 
 // Brand controllers
 router.get("/brand/:id/delete", brand_controller.brand_delete_get);
-router.post("/brand/:id/delete", brand_controller.brand_delete_post);
+router.post(
+  "/brand/:id/delete",
+  authMiddleware,
+  brand_controller.brand_delete_post
+);
 router.get("/brand/:id/update", brand_controller.brand_update_get);
 router.post("/brand/:id/update", brand_controller.brand_update_post);
 router.get("/brand/create", brand_controller.brand_create_get);
